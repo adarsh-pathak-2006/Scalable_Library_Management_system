057389c394fcd59d84d7d7964929278ed8975a0f
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-07 22:55:16 +0530

    added key for cached otp

--- authentication/cache_keys.py
+++ authentication/cache_keys.py
@@ -1,2 +1,5 @@
 def cached_session_key(code):
     return f"cached_session_key_user_code:{code}"
+
+def cached_otp(mobile_no, user_code):
+    return f"otp_sent_on_mobileno:{mobile_no}_with_sessioncode:{user_code}"
--- authentication/tasks.py
+++ authentication/tasks.py
@@ -1,1 +1,10 @@
 from celery import shared_task
+from django.core.cache import cache
+import random
+
+@shared_task
+def OtpGenerationTask(user_code):
+    cached_session=cache.get(user_code)
+    mobile_no=cached_session['mobile_no']
+    generated_otp=random.randint(100000, 999999)
+    return f"mobile_no:{mobile_no}..otp:{generated_otp}"
