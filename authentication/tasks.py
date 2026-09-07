aa4f54751670d2c9b57a216ec3f84ab7fadae428
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-07 22:58:32 +0530

    added background worker for otp generation and also cachekeys to store otp

--- authentication/tasks.py
+++ authentication/tasks.py
@@ -1,10 +1,14 @@
 from celery import shared_task
 from django.core.cache import cache
 import random
+from .cache_keys import cached_otp_key, cached_session_key
+import time
 
 @shared_task
 def OtpGenerationTask(user_code):
-    cached_session=cache.get(user_code)
+    cached_session=cache.get(cached_session_key(user_code))
     mobile_no=cached_session['mobile_no']
+    time.sleep(10)
     generated_otp=random.randint(100000, 999999)
+    cache.set(cached_otp_key(mobile_no=mobile_no, user_code=user_code), generated_otp, timeout=600)
     return f"mobile_no:{mobile_no}..otp:{generated_otp}"
