2e706d89cfad624daaeb2c417a295477d0a17b4e
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-08 16:57:43 +0530

    added apis for books

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from .models import Category, Book
 from rest_framework.response import Response
 from config.pagination import GeneralPagination
@@ -27,4 +28,33 @@ class CategoryAPI(APIView):
             return Response(serial.data, status=200)
         return Response(serial.errors, status=400)
 
-        
+class CategoryDetailAPI(RetrieveUpdateDestroyAPIView):
+    serializer_class=CategorySerializer
+    queryset=Category.objects.all()
+
+class BookAPI(APIView):
+    def get(self, request):
+        page_no=request.query_params.get("page", "1")
+        cached_data=cache.get(books_cache_key(page_no))
+        if cached_data:
+            return Response(cached_data, status=200)
+        paginator=GeneralPagination()
+        data=paginator.paginate_queryset(Book.objects.select_related('category').filter(is_avaliable=True), request, view=self)
+        serial=BookGetSerializer(data, many=True)
+        response=paginator.get_paginated_response(serial.data)
+        cache.set(books_cache_key(page_no), response.data, timeout=300)
+        return response
+
+    def post(self, request):
+        serial=BookWriteSerializer(data=request.data)
+        if serial.is_valid():
+            serial.save()
+            return Response(serial.data, status=200)
+        return Response(serial.errors, status=400)
+
+class BookDetailAPI(RetrieveUpdateDestroyAPIView):
+    def get_serializer_class(self):
+        if self.request.method=='GET':
+            return [BookGetSerializer]
+        return [BookWriteSerializer]
+    queryset=Book.objects.select_related('category').all()
