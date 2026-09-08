057d660ec93005a21f83e3448f6c248e087da003
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-08 18:34:08 +0530

    added models in the lend app

--- authentication/signals.py
+++ authentication/signals.py
@@ -2,10 +2,13 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth import get_user_model
 from .models import Profile
+from lend.models import Cart
 
 User=get_user_model()
 
 @receiver(post_save, sender=User)
 def CreateProfileSignal(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        profile_data=Profile.objects.create(user=instance)
+        Cart.objects.create(user=profile_data)
+        
--- lend/models.py
+++ lend/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from store.models import Book
+from authentication.models import Profile
 
-# Create your models here.
+class Cart(models.Model):
+    user=models.OneToOneField(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.user.username
+
+class CartBook(models.Model):
+    cart=models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='books_in_cart')
+    book=models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField()
+    added_on=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints=models.UniqueConstraint(fields=['book', 'cart'], name='unique_book_in_cart')
+
+    def save(self, *args, **kwargs):
+        if self.quantity > self.book.quantity:
+            raise "selected quantity should be lower than the avaliable stock."
+        return super().save(*args, **kwargs)
+
+class Issue(models.Model):
+    cart=models.ForeignKey(Cart, on_delete=models.CASCADE)
+    issued_on=models.DateTimeField(auto_now_add=True)
+    date_issued=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    
+    
